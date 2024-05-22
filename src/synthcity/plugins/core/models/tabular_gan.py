1fe6f18573a56811b74062bce113790e8ef5267c
anonymous-785 <ashhad.jamiahamdard@gmail.com>
2024-05-22 07:20:18 +0530

    Update tabular_gan.py

--- src/synthcity/plugins/core/models/tabular_gan.py
+++ src/synthcity/plugins/core/models/tabular_gan.py
@@ -191,8 +191,9 @@ class TabularGAN(torch.nn.Module):
             if len(cond.shape) == 1:
                 cond = cond.reshape(-1, 1)
 
-            self.cond_encoder = OneHotEncoder(handle_unknown="ignore").fit(cond)
-            cond = self.cond_encoder.transform(cond).toarray()
+            # self.cond_encoder = OneHotEncoder(handle_unknown="ignore").fit(cond)
+            # cond = self.cond_encoder.transform(cond).toarray()
+            cond = cond
 
             n_units_conditional = cond.shape[-1]
 
@@ -343,7 +344,8 @@ class TabularGAN(torch.nn.Module):
             if len(cond.shape) == 1:
                 cond = cond.reshape(-1, 1)
 
-            cond = self.cond_encoder.transform(cond).toarray()
+            # cond = self.cond_encoder.transform(cond).toarray()
+            cond = cond
 
         if not self.predefined_conditional and self.dataloader_sampler is not None:
             cond = self.dataloader_sampler.get_dataset_conditionals()
@@ -398,7 +400,8 @@ class TabularGAN(torch.nn.Module):
             if len(cond.shape) == 1:
                 cond = cond.reshape(-1, 1)
 
-            cond = self.cond_encoder.transform(cond).toarray()
+            # cond = self.cond_encoder.transform(cond).toarray()
+            cond=cond
 
         if not self.predefined_conditional and self.dataloader_sampler is not None:
             cond = self.dataloader_sampler.sample_conditional(count, p=self.sample_prob)
